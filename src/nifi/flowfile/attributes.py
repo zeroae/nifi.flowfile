5203404b5ecb174448da635d13d2bc2fea79dda5
Patrick Sodré <psodre@gmail.com>
2020-08-31 00:28:16 -0400

    Add Default FlowFile attributes
    ref: https://github.com/apache/nifi/tree/main/nifi-commons/nifi-utils/src/main/java/org/apache/nifi/flowfile/attributes

---
+++ src/nifi/flowfile/attributes.py
@@ -0,0 +1,48 @@
+from uuid import uuid4
+from enum import Enum
+
+
+class CoreAttributes(Enum):
+    """
+    ref: https://github.com/apache/nifi/blob/main/nifi-commons/nifi-utils/src/main/java/org/apache/nifi/flowfile/attributes/CoreAttributes.java
+    """  # noqa: E501
+
+    PATH = "path"
+    ABSOLUTE_PATH = "absolute.path"
+    FILENAME = "filename"
+    UUID = "uuid"
+    PRIORITY = "priority"
+    MIME_TYPE = "mime.type"
+    DISCARD_REASON = "discard.reason"
+    ALTERNATE_IDENTIFIER = "alternate.identifier"
+
+    @staticmethod
+    def create_default_attributes():
+        uuid = str(uuid4())
+        return {
+            CoreAttributes.UUID: uuid,
+            CoreAttributes.PATH: "./",
+            CoreAttributes.FILENAME: uuid,
+        }
+
+
+class FragmentAttributes(Enum):
+    """
+    ref:
+    """
+
+    FRAGMENT_SIZE = "fragment.size"
+    FRAGMENT_ID = "fragment.id"
+    FRAGMENT_INDEX = "fragment.index"
+    FRAGMENT_COUNT = "fragment.count"
+    SEGMENT_ORIGINAL_FILENAME = "segment.original.filename"
+
+
+class SiteToSiteAttributes(Enum):
+    """
+    ref:
+    """
+
+    S2S_HOST = "s2s.host"
+    S2S_ADDRESS = "s2s.address"
+    S2S_PORT_ID = "s2s.port.id"
